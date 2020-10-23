615c209b8284c7e74042be0c9d9fbb4cfda95070
DESKTOP-FU6I8N3\spite <spiteriemma@gmail.com>
2020-10-23 10:55:40 +0200

    Added Lesson 1

---
+++ OOP/Lesson1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+
+            p.IdCard = "383838m";
+            p.Name = "Emma";
+            p.Surname = "Spiteri";
+
+            Console.WriteLine("Please input address: ");
+            p.Address = Console.ReadLine();
+
+            Console.WriteLine(p.DelimitAddress(','));
+            Console.WriteLine("Press a key to terminate app");
+            Console.ReadKey();
+
+        }
+    }
+}
